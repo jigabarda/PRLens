cf2a06bcfb4a83c4fabbba202da2ddf582b835b9
James Ivan Gabarda <jamesivangabarda8@gmail.com>
2025-10-21 21:04:38 +0800

    chore: add tailwind, prisma, and core deps

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // if using Next 13 app router
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
